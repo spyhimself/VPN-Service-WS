1bc3c1f89e4b69f63c7da54162029872d3b3d6b7
sorrow01 <aabderraziq@gmail.com>
2021-05-20 22:11:42 +0000

    Small changes to the signup page, added id so i can get the values of email,username and password and store them into the browser localStorage using its respectable js file

--- source/js/script_signup.js
+++ source/js/script_signup.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+function SaveCreds() {
+    var username = document.getElementById("usrname").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("passwd").value;
+    if (password != document.getElementById("confpasswd").value)
+    {
+        alert("Password missmatch !");
+        window.location.reload();
+    }
+    window.localStorage.setItem("email", email);
+    window.localStorage.setItem("username", username);
+    window.localStorage.setItem("password", password);
+}
