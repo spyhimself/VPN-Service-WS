137a93f5116cbb5561d7b348027eb4434ba1bb2f
sorrow01 <aabderraziq@gmail.com>
2021-05-20 22:23:09 +0000

    Updated the login html and added the js file with the login script and so far it worked, just need now to finish things of and add a simple redirect page to see the userdata ^^

--- source/js/script_signin.js
+++ source/js/script_signin.js
@@ -1,3 +1,16 @@
 function LoadCheckCreds() {
-    
+    var login_username = document.getElementById("username").value;
+    var login_password = document.getElementById("password").value;
+
+    var original_username = window.localStorage.getItem("username");
+    var original_password = window.localStorage.getItem("password");
+
+    if (original_password === login_password && original_username === login_username)
+    {
+        alert("GOT IT RIGHT");  //case if the password is correct: normally this will redirect us to the user account configuration page
+    }
+    else
+    {
+        alert("Fuck you!");     //the password or username is wrong
+    }
 }
