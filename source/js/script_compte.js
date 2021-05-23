ddef2537e61697ebd8fb3a17ea45c6a0e663ec56
sorrow01 <aabderraziq@gmail.com>
2021-05-23 12:46:09 +0100

    the change data thing is finally working

--- source/js/script_compte.js
+++ source/js/script_compte.js
@@ -15,23 +15,25 @@ function change(theid) {
     resettxt();
     var tochange = document.getElementById(theid);
     if (tochange.innerHTML.indexOf("input") == -1)
-    tochange.innerHTML = '<input type="text" placeholder="New ' + theid + '"> <input type="button" value="set" onclick = "SetNew()"> <input type="button" value="cancel" onclick = "resettxt()">';
+    tochange.innerHTML = '<input id = "new' + theid + '" type="text" placeholder="New ' + theid + '"> <input type="button" value="set" onclick = "SetNew()"> <input type="button" value="cancel" onclick = "resettxt()">';
 }
 
 function changedate() {
     resettxt();
-    birthday.innerHTML = '<input type = "date"> <input type="button" value="set" onclick = "SetNew()"> <input type="button" value="cancel" onclick = "resettxt()">';
+    birthday.innerHTML = '<input type = "date" id="newbirthday"> <input type="button" value="set" onclick = "SetNew()"> <input type="button" value="cancel" onclick = "resettxt()">';
 }
 
 function changegen() {
     resettxt();
-    gender.innerHTML = '<input type="radio" name="gender" value="male" checked><b>Male</b> <input type="radio" name="gender" value="female"><b>Female</b> <input type="button" value="set" onclick="SetNew()"> <input type="button" value="cancel" onclick = "resettxt()">';
+    gender.innerHTML = '<input id="newgender" type="radio" name="gender" value="male" checked><b>Male</b> <input type="radio" name="gender" value="female"><b>Female</b> <input type="button" value="set" onclick="SetNew()"> <input type="button" value="cancel" onclick = "resettxt()">';
 }
 
 function SetNew() {
     var tochange = username.innerHTML.indexOf("input") != -1 ? username : password.innerHTML.indexOf("input") != -1 ? password : email.innerHTML.indexOf("input") != -1 
     ? email : birthday.innerHTML.indexOf("input") != -1 ? birthday : gender;
-    
+    if (tochange == gender) window.localStorage.setItem(tochange.id, document.getElementById("new" + tochange.id).checked ? "Male" : "Female" );
+    else window.localStorage.setItem(tochange.id, document.getElementById("new" + tochange.id).value);
+    resettxt();
 }
 
 function resettxt() {
